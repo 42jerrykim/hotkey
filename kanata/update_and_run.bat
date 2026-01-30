55310f68186105730ff15cbb0466b0f65d331f48
Jerry Kim <42jerrykim@gmail.com>
2026-01-30 14:20:49 +0900

    Update `update_and_run.bat` to version v0.8 for improved user feedback

--- kanata/update_and_run.bat
+++ kanata/update_and_run.bat
@@ -27,7 +27,7 @@ set "CONFIG_REPO=42jerrykim/hotkey"
 set "CONFIG_BRANCH=main"
 
 echo ============================================
-echo  Kanata Update and Run Script v0.7
+echo  Kanata Update and Run Script v0.8
 echo ============================================
 echo.
 
